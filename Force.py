e7b840cec517bd6baec8663506a546e5289a2cb4
Escorpio28-Official <80615910+Scorpio28-Official@users.noreply.github.com>
2021-04-13 20:33:43 -0500

    Update Force.py

--- Force.py
+++ Force.py
@@ -1,217 +1,118 @@
-#!/usr/bin/env python
-#-*- coding: iso-8859-1 -*-
-import getopt
-import time
-import os
 import sys
-import datetime
-from random import randint
+import os
+import time
+from itertools import *
 
-version = "1.0.0"
-#Mensaje
-os.system ("clear")
-print("\033[1;32m+\033[1;36m LIVE-GENERATOR")
-time.sleep(1.2)
-#Informacion de ayuda
-def dexter1999():
-    print("\033[1;32m             ______________")
-    print("\033[1;32m       ,===:'.,            `-._")
-    print("\033[1;32m           `:.`---.__         `-._")
-    print("\033[1;32m             `:.     `--.         `.")
-    print("\033[1;32m               \.        `.         `.")
-    print("\033[1;32m       (,,(,    \.         `.   ____,-`.,")
-    print("\033[1;32m     (,'     `/   \.   ,--.___`.'")
-    print("\033[1;32m ,  ,'  ,--.  `,   \.;'         `")
-    print("\033[1;32m `{D, {    \  :    \;")
-    print("\033[1;32m   V,,'    /  /    //")
-    print("\033[1;32m   j;;    /  ,' ,-//.    ,---.      ,")
-    print("\033[1;32m   \;'   /  ,' /  _  \  /  _  \   ,'/")
-    print("\033[1;32m         \   `'  / \  `'  / \  `.' /")
-    print("\033[1;32m          `.___,'   `.__,'   `.__,'")
-dexter1999()
-time.sleep(1.2)
-print("")
-print("\033[1;32m Informatic in Termux")
-print("")
-time.sleep(1.6)
-print(" https://t.me/Informatic_in_Termux")
-print("")
-time.sleep(2.2)
+class colors:
+	am = "\033[1;33m"
+	rj = "\033[91m"
+	ci = "\033[1;36m"
+	vr = "\033[32m"
+	vrf = "\033[42m"
+	f = "\033[0m"
+
+def banner():
+	os.system("clear")
+	sys.stdout.write(colors.vr + '''
+
+         ╔══╗╔═╗╔═╗╔═╗╔═╗╔══╗╔═╗╔═══╗╔═══╗
+         ║ ═╣║╔╝║║║║╬║║╬║╚║║╝║║║║╔═╗║║╔═╗║
+         ╠═ ║║╚╗║║║║╗╣║╔╝╔║║╗║║║╚╝╔╝║║╚═╝║
+         ╚══╝╚═╝╚═╝╚╩╝╚╝─╚══╝╚═╝╔═╝╔╝║╔═╗║
+         ───────────────────────║  ╚═╗╚═╝║
+         C O M U N I D A D      ╚════╚═══╝
+         █████████████████████████████████
+         ┃
+         ┃  ┌══════════════════════┐
+         └═>█''' + colors.f + ''' Informatic in Termux ''' + colors.vr + '''█
+            └══════════════════════┘
+
+         ''' + colors.f + '''Comunidad En Telegram ''' + colors.vr + '''
+         """""""""""""""""""""''' + colors.f + colors.ci + '''
+         https://t.me/Informatic_in_Termux 
+ \n\n''' + colors.f)
+time.sleep(2)                                                            
+	
+
+def lista():
+	global ls
+	global salida
+	ls = []
+	while True:
+		try:
+			salida = input(colors.ci + "Ingrese un Nombre para su diccionario " + colors.f + colors.rj + ">>  " + colors.f) + (".txt")
+			print("")
+			num = int((input(colors.ci + "Cuantos elementos desea combinar?" + colors.f + colors.rj + ">>  " + colors.f)))
+			print("")
+			break
+		except ValueError:
+			print(colors.rj + "\n ERROR Ingrese un Número!!" + colors.f)
+			lista()
+	for i in range (num):
+		print("")
+		x = (input(colors.ci + "Ingrese una palabra" + colors.f + colors.rj + " >>  " + colors.f))
+		ls.append((x))
+		print("")
+	return(ls)
+	return(salida)
+
+def comb():
+	print (colors.vr + "Por favor espere..." + colors.f)
+	print("")
+	time.sleep(2)
+	global perm
+	permutations(ls)
+	perm = list(permutations(ls, 1)), list(permutations(ls, 2)), list(permutations(ls, 3)), list(permutations(ls, 4)), list(permutations(ls, 5)), list(permutations(ls, 6))
+	
+	return perm
+
+def arch():
+	repr(perm)
+	f = open(salida, "w")
+	f.write (repr(perm))
+	f.close()
 
 
-#Informacion de ayuda
-def usage():
-    print("+\033[1;37m Metodo de uso\033[1;31m +")
-    print("")
-    print("\033[1;33m  python2 Live-Generator.py -b \033[1;31m >>>\033[1;33m Opciones de uso")
-    print("\033[1;33m  python2 Live-Generator.py -h \033[1;31m >>>\033[1;33m Mensaje de ayuda")
-    print("")
-    print("+\033[;32m Opciones de uso\033[1;31m +")
-    print("")
-    print("\033[1;32m -b, \033[1;31m >>>\033[1;32m -bin\033[1;31m >>>\033[1;32m Formato de bin")
-    print("\033[1;32m -u, \033[1;31m >>>\033[1;32m -cantidad\033[1;31m >>>\033[1;32m tarjetas a generar")
-    print("\033[1;32m -c, \033[1;31m >>>\033[1;32m -ccv\033[1;31m >>>\033[1;32m Genera ccv al azar")
-    print("\033[1;32m -d, \033[1;31m >>>\033[1;32m -date\033[1;31m >>>\033[1;32m Genera fechas al azar")
-    print("")
-    print("+\033[;33m Ejemplo de uso\033[1;31m +")
-    print("")
-    print("\033[1;37m EL USO DE ESTA HERRAMIENTA\033[0m")
-    print("\033[1;37m ES RESPONSABILIDAD DE QUIEN LA UTILICE")
-    print("")
-    print("\033[1;36mpython2 Live-Generator.py -b 123456xxxxxxxxxx -u 30 -d -c ")
-    print("")
-#Arg parser
-def parseOptions(argv):
-    bin_format = ""
-    saveopt = False
-    limit = 30
-    ccv = False
-    date = False
-    check = False
+def conf():
+	print ("\n".join([str(x) for x in ls]))
+	print("")
+	conf = (input(colors.am + 'Desea agregar más elementos? ' + colors.f + colors.vr + '[s/n]' + colors.f + colors.rj + ' >>  ' + colors.f))
+	if conf == 's':
+		iniciar()
+	else:
+		iniciar2()
+def edit():
+	ar = open(salida, "r")
+	cad = ar.read().replace('\n','').replace('-', ' ')
+	salto = ")"
+	simbolo = "][(,' "
+	for carac in salto:
+		cad = cad.replace(carac,"\n")
+	for carac in simbolo:
+		cad = cad.replace(carac,"")
+	
+	def cambiAr():
+		f = open(salida, "w")
+		f.write (cad)
+		f.close()
 
-    try:
-        opts, args = getopt.getopt(argv, "h:b:u:gcd",["help", "bin", "guardar", "cantidad", "ccv", "date"])
-        for opt, arg in opts:
-            if opt in ("-h"):
-                usage()
-                sys.exit()
-            elif opt in ("-b", "-bin"):
-                bin_format = arg
-            elif opt in ("-g", "-guardar"):
-                saveopt = True
-            elif opt in ("u", "-cantidad"):
-                limit = arg
-            elif opt in ("-c", "-ccv"):
-                ccv = True
-            elif opt in ("-d", "-date"):
-                date = True
+	cambiAr()
+	print(colors.vrf + "Proceso Terminado!!\n" + colors.f)
+	print("")
+	time.sleep(2)
+	print(colors.vr + '\nVerifique el archivo "' + salida + '"' + colors.f)
+	print("")
+	
+def iniciar():
+	banner()
+	lista()
+	conf()
 
-        return(bin_format, saveopt, limit, ccv, date)
+def iniciar2():
+	comb()
+	arch()
+	banner()
+	edit()
+	
 
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-
-#CHECKER BASADO EN ALGORITMO LUHN
-def cardLuhnChecksumIsValid(card_number):
-    """ checks to make sure that the card passes a luhn mod-10 checksum """
-
-    sum = 0
-    num_digits = len(card_number)
-    oddeven = num_digits & 1
-
-    for count in range(0, num_digits):
-        digit = int(card_number[count])
-
-        if not (( count & 1 ) ^ oddeven ):
-            digit = digit * 2
-        if digit > 9:
-            digit = digit - 9
-
-        sum = sum + digit
-
-    return ( (sum % 10) == 0 )
-
-#GENERA UNA BASE DE BIN XXXXXXXXXXXXXXXX
-def ccgen(bin_format):
-    out_cc = ""
-    if len(bin_format) == 16:
-        #Iteration over the bin
-        for i in range(15):
-            if bin_format[i] in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9"):
-                out_cc = out_cc + bin_format[i]
-                continue
-            elif bin_format[i] in ("x"):
-                out_cc = out_cc + str(randint(0,9))
-            else:
-                print("\nCaracter no valido en el formato: {}\n".format(bin_format))
-                print("El formato del bin es: xxxxxxxxxxxxxxxx de 16 digitos\n")
-                print("Ayuda: python2 Live-Generator.py -h \n")
-                sys.exit()
-
-        #Generate checksum (last digit) -- IMPLICIT CHECK
-        for i in range(10):
-            checksum_check = out_cc
-            checksum_check = checksum_check + str(i)
-
-            if cardLuhnChecksumIsValid(checksum_check):
-                out_cc = checksum_check
-                break
-            else:
-                checksum_check = out_cc
-
-    else:
-        print("\033[1;32m")
-        print("\nERROR: Inserta un bin valido\n")
-        print("SOLUCION: El formato del bin es: xxxxxxxxxxxxxxxx de 16 digitos\n")
-        print("AYUDA: python2 Live-Generator.py -h\n")
-        sys.exit()
-
-    return(out_cc)
-
-#Write on a file that takes a list for the argument
-def save(generated):
-    now = datetime.datetime.now()
-    file_name = "cc-gen_output_{0}.txt".format(str(now.day) + str(now.hour) + str(now.minute) + str(now.second))
-    f = open(file_name, 'w')
-    for line in generated:
-        f.write(line + "\n")
-    f.close
-
-#Random ccv gen
-def ccvgen():
-    ccv = ""
-    num = randint(10,999)
-
-    if num < 100:
-        ccv = "0" + str(num)
-    else:
-        ccv = str(num)
-
-    return(ccv)
-
-#Random exp date
-def dategen():
-    now = datetime.datetime.now()
-    date = ""
-    month = str(randint(1, 12))
-    current_year = str(now.year)
-    year = str(randint(int(current_year[-2:]) + 1, int(current_year[-2:]) + 6))
-    date = month + "|" + year
-
-    return date
-
-#The main function
-def main(argv):
-    bin_list = []
-    #get arg data
-    (bin_format, saveopt, limit, ccv, date) = parseOptions(argv)
-    if bin_format is not "":
-        for i in range(int(limit)):
-            if ccv and date:
-                bin_list.append(ccgen(bin_format) + "|" + ccvgen() + "|" + dategen())
-                print(bin_list[i])
-            elif ccv and not date:
-                bin_list.append(ccgen(bin_format) + "|" + ccvgen())
-                print(bin_list[i])
-            elif date and not ccv:
-                bin_list.append(ccgen(bin_format) + "|" + dategen())
-                print(bin_list[i])
-            elif not date and not ccv:
-                bin_list.append(ccgen(bin_format))
-                print(bin_list[i])
-
-        if not bin_list:
-            print("\nERROR: el bin que insertaste no es valido\n")
-        else:
-            print("\n Todas las tarjetas fueron validadas con exito ")
-            print("\n Pueden ser usadas satisfactoriamente")
-
-        if saveopt:
-            save(bin_list)
-    else:
-        usage()
-        sys.exit()
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+iniciar()
